5cec281dd0abb3e4f85e12d886b7c2ae70a95eea
Mateus Alencar <111691429+Mateus-Alencar@users.noreply.github.com>
2025-05-28 19:44:45 -0300

    Create TestaCliente.java

---
+++ Loja/TestaCliente.java
@@ -0,0 +1,7 @@
+
+public class TestaCliente {
+    public static void main(String[] args){
+        Cliente e1 = new Cliente(1,"Omerio","Rua número 0", 1.2, 3.4);
+        System.out.println("Valor de e1: "+e1);
+    }
+}
