7014961d0641cab54db8e2f478ed4c653d159366
Mateus Alencar <111691429+Mateus-Alencar@users.noreply.github.com>
2025-05-28 20:55:35 -0300

    Update Carrinho.java

--- Loja/Carrinho.java
+++ Loja/Carrinho.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Carrinho {
-    private int idCarinho;
+    private int idCarrinho;
     private Cliente cliente;
     private String data;
     private String situacao;
@@ -16,7 +16,7 @@ public class Carrinho {
     }
 
     public int getIdCarinho() {
-        return idCarinho;
+        return idCarrinho;
     }
 
     public Cliente getCliente() {
@@ -47,6 +47,87 @@ public class Carrinho {
         } else {
             System.out.print("O carrinho já estava fechado anteriormente.");
             return false;
+        }
+    }
+    public void adicionarItem(Produto p, double quantidade, double preco){
+        ItemCarrinho ic;
+        if (situacao.equalsIgnoreCase("Fechada")){
+            System.out.print("Não é possível adicionar itens no carrinho.");
+            return;
+        } 
+        if (existeProduto(p.getIdProduto())){
+            System.out.print("Produto já anteriormente incluído no carrinho");
+            return;
+        }
+        ic = new ItemCarrinho(p, quantidade, preco);
+        itens.add(ic);
+        cliente.setDebito(cliente.getDebito()+p.getPreco()*quantidade);
+        System.out.print("Item incluído no carrinho!");
+    }
+    public boolean existeProduto(int id){
+        for (ItemCarrinho ic: itens){
+            if (ic.getProduto().getIdProduto()==id){
+                return true;
+            }
+            return false;
+        }
+    }
+    // Método para percorrer a ArrayList e mostrar o seu conteúdo
+    public void mostrarCarrinho (){
+        for (ItemCarrinho l : itens){
+            System.out.print(l);
+        }
+    }
+    public double mostrarValorTotal(){
+        double contador = 0;
+        for (ItemCarrinho ic: itens){
+            contador =contador + ic.getQuantidade() * ic.getPreco();
+        }
+        return contador;
+    }
+    
+    //Método para mostrar o pedido
+    public void mostraDadosCarrinho(){
+        System.out.println("Id Pedido: "+ idCarrinho);
+        System.out.println("Cliente: "+ cliente);
+        System.out.println("Data: "+data);
+        System.out.println("Situação: "+ situacao);
+        System.out.println("Total carrinho: " + mostrarValorTotal());
+    }
+    public int obtemPosicaoItem(int id){
+        for (int i = 0; i<itens.size();++i){
+            if(itens.get(i).getProduto().getIdProduto()==id){
+                return i;
+            }
+            return -1;
+        }
+    }
+    //Criar um metodo para alterar a quantidade de um produto no carrinho
+    public void alterarQuantProd(int idProduto, double quant, boolean tipo){
+        // o tipo se for true, acrescenta a quantidade do item
+        // se o tipo for false, decrementa a quantidade do item
+        int pos;
+        pos = obtemPosicaoItem(id);
+        if(pos==-1){
+            System.out.print("O id informado não foi encontrado");
+            return;
+        }
+        if(tipo){
+            //Adicionando
+            itens.get(pos).setQuantidade(itens.get(pos).getQuantidade()+quant);
+            cliente.setDebito(cliente.setDebito()+itens.get(pos).getPreco()*quant);
+        }else{
+            //Retirando
+            if(quant>itens.get(pos).getQuantidade()){
+                System.out.println("Quantidade a ser retirada é mairo que a quantidade incluida");
+                return;
+            }
+            itens.get(pos).setQuantidade(itens.get(pos).getQuantidade()- quant);
+            cliente.setDebito(cliente.setDebito()+itens.get(pos).getPreco()*quant);
+            if(itens.get(pos).getQuantidade()==0){
+                itens.remove(pos);
+                System.out.pritln("Item removido do carrinho");
+            }
         }
     }
 }
